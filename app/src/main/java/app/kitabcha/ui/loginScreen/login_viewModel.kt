46002193af740c7e066c98291145c8dc55b40e83
MKZ3R0 <bscs22021@itu.edu.pk>
2024-05-04 19:29:36 +0500

    added view model

--- app/src/main/java/app/kitabcha/ui/loginScreen/login_viewModel.kt
+++ app/src/main/java/app/kitabcha/ui/loginScreen/login_viewModel.kt
@@ -1,14 +1,32 @@
 package com.mkrdeveloper.viewmodeljetpack.app.kitabcha.ui.loginScreen
 
+import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import app.kitabcha.data.MainDatabase
+import app.kitabcha.data.User
+import app.kitabcha.ui.Repository.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class login_viewModel {
-    var name_of_User:String=""
+class login_viewModel(application: Application): AndroidViewModel(application) {
 
 
+    private val repository: Repository
 
+    init{
+        val userdao=MainDatabase.getDataBase(application).userDao()
+        repository=Repository(userdao)
+    }
+
+    fun addUser(user: User){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.addUser(user)
+        }
+    }
 }
 
