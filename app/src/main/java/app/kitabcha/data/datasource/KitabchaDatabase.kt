cebcb500a77b1e72c11bf8c4c01ada2441b94814
MKZ3R0 <bscs22021@itu.edu.pk>
2024-05-08 13:52:37 +0500

    improved repos

--- app/src/main/java/app/kitabcha/data/datasource/KitabchaDatabase.kt
+++ app/src/main/java/app/kitabcha/data/datasource/KitabchaDatabase.kt
@@ -2,6 +2,8 @@ package com.mkrdeveloper.viewmodeljetpack.app.kitabcha.data.datasource
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import app.kitabcha.data.datasource.LibraryDao
+import app.kitabcha.data.datasource.MangaDao
 import app.kitabcha.data.entity.UserEntity
 import app.kitabcha.data.datasource.UserDao
 
@@ -12,4 +14,6 @@ import app.kitabcha.data.datasource.UserDao
 )
 abstract class KitabchaDatabase : RoomDatabase() {
     abstract val userDao: UserDao
+    abstract val mangaDao: MangaDao
+    abstract val libraryDao: LibraryDao
 }
