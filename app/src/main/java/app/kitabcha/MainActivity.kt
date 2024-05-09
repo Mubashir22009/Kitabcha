97474f29585645050a154d77c8f4cb9c1c539711
Tayyab ur Rehman <tayyabu207@gmail.com>
2024-05-09 14:17:57 +0500

    nav refactor files
    thanks Mubashir

--- app/src/main/java/app/kitabcha/MainActivity.kt
+++ app/src/main/java/app/kitabcha/MainActivity.kt
@@ -8,27 +8,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import app.kitabcha.nav.NavCont
 import app.kitabcha.ui.theme.KitabchaTheme
-import com.mkrdeveloper.viewmodeljetpack.app.kitabcha.presentation.LoginScreen
 import dagger.hilt.android.AndroidEntryPoint
-import app.kitabcha.nav.navCont
-import app.kitabcha.nav.Routes
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            navCont()
-            /* KitabchaTheme {
+            KitabchaTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
-                }*/
-
-
+                    NavCont()
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/app/kitabcha/nav/NavController.kt
@@ -0,0 +1,25 @@
+package app.kitabcha.nav
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mkrdeveloper.viewmodeljetpack.app.kitabcha.presentation.LoginScreen
+
+@Composable
+fun NavCont()
+{
+  val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Routes.LoginScreen, builder = {
+      composable(Routes.LoginScreen)
+      {
+        LoginScreen(navController)
+      }
+      /*composable(Routes.secondScreen)
+      {
+            secondPage(navController)
+      }*/
+
+    } )
+
+}
