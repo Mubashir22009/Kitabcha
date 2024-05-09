0bf59c7d4321d686f84abd413cb869309d6c4d3f
Tayyab ur Rehman <tayyabu207@gmail.com>
2024-05-09 22:03:21 +0500

    routes and navcont

---
+++ app/src/main/java/app/kitabcha/navcont/Routes.kt
@@ -0,0 +1,6 @@
+package app.kitabcha.navcont
+
+object Routes {
+    var LoginScreen = "loginscreen"
+    var library="library"
+}
---
+++ app/src/main/java/app/kitabcha/navcont/navController.kt
@@ -0,0 +1,35 @@
+package app.kitabcha.navcont
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import app.kitabcha.ui.theme.KitabchaTheme
+import com.mkrdeveloper.viewmodeljetpack.app.kitabcha.presentation.LoginScreen
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.navigation.compose.composable
+
+@Composable
+fun navCont()
+{
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Routes.LoginScreen, builder = {
+        composable(Routes.LoginScreen,)
+        {
+            LoginScreen(navController)
+        }
+        /*composable(Routes.secondScreen)
+        {
+              secondPage(navController)
+        }*/
+
+    })
+
+}
