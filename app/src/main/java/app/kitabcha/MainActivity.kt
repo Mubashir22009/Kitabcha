c9c57913edbff8abee7eeea2c8f7f8222f741f07
Tayyab ur Rehman <130135484+Tayyab-ur-Rehman@users.noreply.github.com>
2024-05-10 11:13:26 +0500

    Nav Controller
    * routes and navcont
    
    * routes and navcont final done

--- app/src/main/java/app/kitabcha/MainActivity.kt
+++ app/src/main/java/app/kitabcha/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import app.kitabcha.navcont.navCont
 import app.kitabcha.ui.theme.KitabchaTheme
 import com.mkrdeveloper.viewmodeljetpack.app.kitabcha.presentation.LoginScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    navCont()
                 }
             }
         }
