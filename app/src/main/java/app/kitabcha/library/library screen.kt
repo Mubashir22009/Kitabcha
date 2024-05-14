e510ff1e9a6a74e797825d8cea79ee373afcc256
Tayyab ur Rehman <tayyabu207@gmail.com>
2024-05-14 14:36:49 +0500

    library screen and viewmodel progress 87% removed warnings

--- app/src/main/java/app/kitabcha/library/library screen.kt
+++ app/src/main/java/app/kitabcha/library/library screen.kt
@@ -1,60 +1,28 @@
 package app.kitabcha.library
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import com.mkrdeveloper.viewmodeljetpack.app.kitabcha.library.libraryScreenViewModel
-import com.mkrdeveloper.viewmodeljetpack.app.kitabcha.presentation.Content
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import  androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalMapOf
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import app.kitabcha.data.entity.CategoryEntity
 import app.kitabcha.data.entity.UserEntity
 
 
@@ -79,8 +47,8 @@ fun Content(libraryViewModel: libraryScreenViewModel, navController: NavControll
 
          )
          {
-             index,CategoryE ->
-             Text(text = index.toString() + "    " +CategoryE[index].catTitle ,
+             index,categoryE ->
+             Text(text = index.toString() + "    " +categoryE[index].catTitle ,
                  modifier = Modifier  .clickable { // call to another screen getting a list<MangaEntity>
                   })
 
