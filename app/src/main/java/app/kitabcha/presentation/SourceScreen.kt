570755cbb313a1401b42814136281174b79a31e1
MKZ3R0 <bscs22021@itu.edu.pk>
2024-05-14 17:27:37 +0500

    added source screen, deleted interceptor

--- app/src/main/java/app/kitabcha/presentation/SourceScreen.kt
+++ app/src/main/java/app/kitabcha/presentation/SourceScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -49,7 +48,7 @@ fun SourceItem(
 
 
 @Composable
-fun SourceLazyColumn(
+fun SourceScreen(
     userId: Int,
     navController: NavController
 ) {
