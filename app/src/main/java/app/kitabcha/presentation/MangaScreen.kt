e5930f4363d8e4e248bef59973a1a5260784397a
Mubashir22009 <bscs22009@itu.edu.pk>
2024-06-08 16:35:00 +0500

    cleanup mangascreen and open reader :D

--- app/src/main/java/app/kitabcha/presentation/MangaScreen.kt
+++ app/src/main/java/app/kitabcha/presentation/MangaScreen.kt
@@ -1,30 +1,24 @@
 package app.kitabcha.presentation
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import app.kitabcha.navigation.Routes
 
 @Composable
 fun MangaScreen(
@@ -55,66 +49,48 @@ fun MangaScreenContent(
         }
     }
 
-    if (mangaChapters.isNotEmpty()) {
-        LazyColumn(
+    Scaffold { pad ->
+        Box(
             modifier =
                 Modifier
-                    .padding(top = 100.dp)
-                    .padding(bottom = 30.dp),
+                    .padding(bottom = pad.calculateBottomPadding())
+                    .padding(top = pad.calculateTopPadding())
+                    .fillMaxSize(),
         ) {
-            itemsIndexed(
-                mangaChapters,
-            ) { _, mangaChap ->
+            if (mangaChapters.isNotEmpty()) {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(
+                        mangaChapters,
+                    ) { mangaChap ->
+                        val num =
+                            if (mangaChap.chapterNum.toInt().toFloat() != mangaChap.chapterNum) {
+                                mangaChap.chapterNum
+                            } else {
+                                mangaChap.chapterNum.toInt()
+                            }
+
+                        Row(
+                            Modifier
+                                .padding(15.dp)
+                                .fillMaxSize(),
+                        ) {
+                            Text(
+                                text = num.toString(),
+                                modifier =
+                                    Modifier
+                                        .clickable {
+                                            navController.navigate("${Routes.readerScreen}/${mangaChap.chapterID}/${manga!!.sourceID}")
+                                        },
+                            )
+                        }
+                    }
+                }
+            } else {
                 Text(
-                    text = " -    chapter no." + mangaChap.chapterNum + "  owner manga id = " + mangaChap.ownerMangaID,
-                    style = TextStyle(fontWeight = FontWeight.Bold),
-                    fontSize = (20.sp),
-                    modifier =
-                        Modifier
-                            .padding(20.dp)
-                            .clickable {
-                                // call to another screen opening manga :  reader
-                            },
+                    text = "Empty",
+                    modifier = Modifier.align(Alignment.Center),
                 )
             }
         }
-    } else {
-        Text(text = " Empty ", modifier = Modifier.padding(20.dp))
-    }
-    Row(
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.Bottom,
-        modifier =
-            Modifier
-                .fillMaxSize().padding(bottom = 15.dp),
-    ) {
-        Button(
-            onClick = { },
-            modifier = Modifier.fillMaxWidth(),
-            shape = MaterialTheme.shapes.extraLarge, // Adjust shape as needed
-            colors = ButtonDefaults.buttonColors(Color.Yellow, contentColor = Color.Black),
-        ) {
-            Text(
-                text = "Button if needed",
-            )
-        }
-    }
-    Row(
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.Bottom,
-        modifier =
-            Modifier
-                .fillMaxSize().padding(bottom = 15.dp),
-    ) {
-        Text(
-            // text = "${manga.mangaTitle}        Author = ${manga.mangaAuthor}", after getting manga entity by manga id
-            text = "                            information   ",
-            style = TextStyle(fontWeight = FontWeight.Bold),
-            fontSize = (40.sp),
-            modifier =
-                Modifier
-                    .padding(15.dp),
-            color = Color.Yellow,
-        )
     }
 }
